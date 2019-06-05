045ce854a90fc13579ef10c94eccaad23989fd13
yyl <772788236@qq.com>
2019-06-05 15:28:38 +0800

    修改找出被13整除的数并存到数组中

--- src/com/java24hours/FindNum.java
+++ src/com/java24hours/FindNum.java
@@ -3,10 +3,14 @@ package com.java24hours;
 public class FindNum {
 
 	public static void main(String[] args) {
+		int[] num=new int[300];
 		int i=0;
+		int j=0;
 		while(true) {
 			if(i%13==0) {
-				System.out.println(i);	
+				System.out.println(i);
+				num[j]=i;
+				j++;
 			}
 			else if (i>400) {
 				break;
